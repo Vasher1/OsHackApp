1e6fc51581896d8d6f0871a5aa84b5c0eddad790
Alan Ditchfield <aditchfield1@ombudsman-services.org>
2017-10-26 15:01:25 +0100

    Fixed Android Maps API Key

--- OSHackApp/MainActivity.cs
+++ OSHackApp/MainActivity.cs
@@ -1,12 +1,18 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Gms.Maps;
 
 namespace OSHackApp
 {
     [Activity(Label = "OSHackApp", MainLauncher = true)]
-    public class MainActivity : Activity
+    public class MainActivity : Activity, IOnMapReadyCallback
     {
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            throw new System.NotImplementedException();
+        }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
