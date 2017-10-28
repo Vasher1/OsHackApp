c9ee632e8bf1ab3893d7a5dfa7272119bc046526
Alan Ditchfield <aditchfield1@ombudsman-services.org>
2017-10-28 15:10:18 +0100

    Barcode Scanny Stuff

--- OSHackApp/OSHackApp/MainActivity.cs
+++ OSHackApp/OSHackApp/MainActivity.cs
@@ -1,18 +1,29 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using ZXing.Mobile;
+using Android.Views;
 
 namespace OSHackApp
 {
     [Activity(Label = "OSHackApp", MainLauncher = true)]
-    public class MainActivity : Activity
+    public class MainActivity : Activity 
     {
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            Button scanButton = (Button)FindViewById(Resource.Id.buttonScan);
+            //TextView text = (TextView)FindViewById(Resource.Id.textView1);
+
+            scanButton.Click += async (sender, e) =>
+            {
+                var scanner = new MobileBarcodeScanner();
+                var result = await scanner.Scan();
+                //text.Text = result.Text;
+            };
         }
     }
 }
